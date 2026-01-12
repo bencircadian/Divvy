d80677bc93fe0ddd42f7183944fe4addb1014e94
bencircadian <ben.circadian@gmail.com>
2026-01-12 00:23:59 +0000

    Bump version to 6

--- lib/services/version_service.dart
+++ lib/services/version_service.dart
@@ -15,7 +15,7 @@ class VersionService {
   VersionService._internal();
 
   /// Current app build number (increment this with each deploy).
-  static const int currentBuildNumber = 5;
+  static const int currentBuildNumber = 6;
 
   /// Stream controller for update availability.
   final _updateAvailableController = StreamController<bool>.broadcast();
