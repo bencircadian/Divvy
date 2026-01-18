fba1ec6de3271611765040306b0239dd4df96a1a
bencircadian <ben.circadian@gmail.com>
2026-01-18 21:27:56 +0000

    Build: Deploy with more debug logging

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2402930264"
+    serviceWorkerVersion: "2103786881"
   }
 });
